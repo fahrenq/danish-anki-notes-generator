b6c90fa752d880ffb811f9b2b922bd5f5d73fc56
fahrenq <ivankuznetsov316@gmail.com>
2020-10-21 21:31:53 +0200

    Finished Anki Deck generator

---
+++ generate_anki_deck.py
@@ -0,0 +1,116 @@
+from get_word_info import get_word_info
+import genanki
+import requests
+
+
+class QuestionOnlyHashNote(genanki.Note):
+    @property
+    def guid(self):
+        return genanki.guid_for(self.fields[0])
+
+
+deck = genanki.Deck(2059400110, 'Danish')
+
+package = genanki.Package(deck)
+package.media_files = []
+
+css = """
+.card {
+    font-family: verdana;
+    font-size: 20px;
+    text-align: center;
+    color: black;
+    background-color: white;
+}
+
+.metainfo {
+    font-size: 10px;
+    margin-bottom: 10px;
+}
+
+.bojning {
+    padding: 10px;
+    background-color: aliceblue;
+    color: black;
+    font-size: 15px;
+    margin-bottom: 10px;
+}
+
+.answer {
+    margin-bottom: 10px;
+}
+
+.example {
+    font-size: 15px;
+}
+"""
+afmt = """
+{{FrontSide}}
+<hr id="answer">
+<div class="metainfo">
+    {{PartOfSpeech}} {{Køn}}
+</div>
+<div class="bojning">
+    {{Bøjning}}
+</div>
+<div class="answer">
+    {{Answer}}
+</div>
+<div class="example">
+    {{Example}}
+</div>
+<br>
+{{Media}}
+"""
+
+model = genanki.Model(1091735104, 'Danish',
+                      fields=[
+                          {'name': 'Question'},
+                          {'name': 'Bøjning'},
+                          {'name': 'PartOfSpeech'},
+                          {'name': 'Køn'},
+                          {'name': 'Example'},
+                          {'name': 'Answer'},
+                          {'name': 'Media'},
+                      ],
+                      css=css,
+                      templates=[
+                          {
+                              'name': 'Card 1',
+                              'qfmt': '{{Question}}',
+                              'afmt': afmt
+                          },
+                      ])
+
+if __name__ == '__main__':
+    with open('./words.csv') as f:
+        for line in f.readlines():
+            tokenised = line.strip().split('|')
+            if len(tokenised) == 2:
+                [word, translation] = tokenised
+                example = ""
+            elif len(tokenised) == 3:
+                [word, translation, example] = tokenised
+
+            info = get_word_info(word)
+
+            # audio
+            audio_url = info.get('audio')[0]
+            filename = audio_url.split('/')[-1]
+            r = requests.get(audio_url)
+            open('media/' + filename, 'wb').write(r.content)
+            package.media_files.append('media/'+filename)
+
+            note = QuestionOnlyHashNote(model=model, fields=[
+                word,
+                '||'.join(info.get('bojning')),
+                info.get('part_of_speech'),
+                info.get('kon'),
+                example,
+                translation,
+                f'[sound:{filename}]'
+            ])
+            deck.add_note(note)
+            print(f'Added note for "{word}"')
+
+    package.write_to_file('Danish.apkg')
---
+++ get_word_info.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+
+BASE_URL = "https://ordnet.dk/ddo/ordbog?query="
+
+
+def get_word_info(word):
+    link = BASE_URL + word
+    html = requests.get(link).text
+    soup = BeautifulSoup(html, 'html.parser')
+    bojning_elements = soup.select('#id-boj .tekstmedium')
+    bojning = list(map(lambda x: x.strip(), bojning_elements[0].get_text().split(
+        '\xa0'))) if len(bojning_elements) > 0 else None
+    audio = list(map(lambda x: x.get('href'), soup.select('#id-udt audio a')))
+    definition_elements = soup.select(
+        '.definitionBoxTop .allow-glossing')[0].get_text().strip().split(',')
+    part_of_speech = definition_elements[0]
+    kon = definition_elements[1].strip() if part_of_speech == 'substantiv' else None
+
+    return {
+        'word': word,
+        'bojning': bojning,
+        'audio': audio,
+        'part_of_speech': part_of_speech,
+        'kon': kon
+    }
+
+
+if __name__ == '__main__':
+    r = get_word_info('syd')
+    print(r)
